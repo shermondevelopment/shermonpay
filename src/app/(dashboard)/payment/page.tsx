3f196bdb10bbf14bbcc31f2e7aed7b207eb1de8d
shermondevelopment <victorshermon@yahoo.com>
2024-08-07 21:38:54 -0300

    feat: refreshing data

--- src/app/(dashboard)/payment/page.tsx
+++ src/app/(dashboard)/payment/page.tsx
@@ -1,9 +1,18 @@
 import prisma from "@/settings/prisma";
 import PaymentTable from "./components/paymenttaable";
 
+export const relalidate = 0;
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+async function getPayments() {
+	return await prisma.payment.findMany()
+}
+
 export default async function Payment() {
 
-	const payments = await prisma.payment.findMany()
+	const payments = await getPayments()
 
 	return (
 		<>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from 'primereact/button'
+import Link from 'next/link';
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+
+
   return (
     <main>
       <h1>Home</h1>
       <Button>
-         Im Buttom
+           <Link href="/payment">
+             Im Buttom
+           </Link>
       </Button>
     </main>
   );
