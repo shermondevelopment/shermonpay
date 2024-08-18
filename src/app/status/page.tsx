c2c5d733541e2ee18231d42c06417af6b074e0c4
shermondevelopment <victorshermon@yahoo.com>
2024-08-18 11:49:24 -0300

    fix: update status page

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,5 +1,7 @@
 export default function Status() {
 	return (
-		<h4>Pagamento aprovado</h4>
+		<div className="w-screen h-screen flex justify-content-start align-items-center">
+			<h4>Pagamento aprovado</h4>
+		</div>
 	)
 }
