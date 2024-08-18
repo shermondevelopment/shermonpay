81bf05be8989b461c2861950cde352952587a2fc
shermondevelopment <victorshermon@yahoo.com>
2024-08-18 16:37:11 -0300

    feat: list account receive in form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
+import "./globals.css";
+import type { Metadata } from "next";
+
 
 
 export const metadata: Metadata = {
